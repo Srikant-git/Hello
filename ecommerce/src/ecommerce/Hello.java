eca093151971bed268336cd1d48b8733092eac65
ay031083 <ay031083@BB9P5H2.Broadcom.net>
2019-09-30 00:04:10 +0530

    Hello code first commit

---
+++ ecommerce/src/ecommerce/Hello.java
@@ -0,0 +1,14 @@
+package ecommerce;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//print hello world
+		
+		System.out.println("Hello world");
+
+	}
+
+}
